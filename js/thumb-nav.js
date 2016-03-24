331097a5bb87a5f2fa6f3fe79d3e35b041172cc5
Amy Eckert <amy@amyeckertphoto.com>
2016-03-24 16:18:22 -0400

    prev and next working until I get to the last or first

--- js/thumb-nav.js
+++ js/thumb-nav.js
@@ -4,6 +4,50 @@
 $(document).ready(function(){
 
 
+// _______PREVIOUS OR NEXT IMAGE Navigation ______________
+
+// step 1- previous or next button clicked?
+// step 2 find the active item
+
+//step 3a if prev triggers a click on .item isActive previous sibling, 
+//step 3a if next triggers a click on .item isActive next sibling, 
+// 		3b if isActive is first in list, then find last .item
+// 	 	3c if next, trigger click on .item isActive nextSibling.
+//      3d if isActive is last in list then find first .item
+
+$('.previous').on('click', function() {
+	// makes big image visible
+	var bigImg = document.getElementById('bigImg');
+	bigImg.style.visibility = 'visible';
+
+	var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
+	// step 2 find the active item and chose next sibling
+	var prevImg = document.querySelector('div.isActive').previousElementSibling;
+	$('.item.isActive').removeClass('isActive');
+	$(prevImg).addClass('isActive');
+
+	var chosen_thumb = $(prevImg).find('img').attr('src');
+
+	$('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+
+});
+
+$('.next').on('click', function() {
+	// makes big image visible
+	var bigImg = document.getElementById('bigImg');
+	bigImg.style.visibility = 'visible';
+
+	var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
+	 // step 2 find the active item and chose next sibling
+	var nextImg = document.querySelector('div.isActive').nextElementSibling;
+	$('.item.isActive').removeClass('isActive');
+	$(nextImg).addClass('isActive');
+	var chosen_thumb = $(nextImg).find('img').attr('src');
+
+	//makes that img new big img.
+	$('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+
+});
 
 /*-------------------------------------------------------------------------------------------------
 thumbnail picker
@@ -18,11 +62,9 @@ $('.item').on('click', function(e) {
 	var chosen_thumb = $(this).find('img').attr('src');
 	console.log(chosen_thumb);
 
-
-	// $('.big-img-container').attr('style', {'background-image':chosen_thumb});
 	$('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
 
-	//change visibility of  .thumbs to hidden
+	//change visibility of .thumbs to hidden
 	var thumbs = document.getElementById('thumbNav');
 	//____________________________________________________________________________________
 	//specify breakpoint where this happens if minwidth<= xpx (display=flex) else {display=none}
@@ -35,14 +77,9 @@ $('.item').on('click', function(e) {
 	var imgInfo = document.getElementById('imgInfo');
 	imgInfo.style.visibility = 'visible';
 
-	// only display Image Info div when a thumb is chosen. HIDE div when showing 
-	// thumbnail view hide by changing color to #aba7a7, when visible.
-
 });	
 
-
-
-// Hide Image info div when "thumbnails" button is clicked
+// Hides Image info div when "thumbnails" button is clicked
 $('.thumbs-only').on('click', function() {
 
 	var bigImg = document.getElementById('bigImg');
@@ -51,21 +88,69 @@ $('.thumbs-only').on('click', function() {
 
 	bigImg.style.visibility = 'hidden';
 	showThumbs.style.display = 'block';
-
 	imgInfo.style.visibility = 'hidden';
 })
 
 
+//https://www.christianheilmann.com/2015/04/08/keeping-it-simple-coding-a-carousel/ :
+// carousel = (function(){
+//   //find buttons for next/prev"
+//   var box = document.querySelector('div.carousel-nav');
+//   var next = document.querySelector('button.nextImg');
+//   var prev = document.querySelector('button.prevImg');
 
-// _______PREVIOUS OR NEXT IMAGE:______________
+//   //finds container with images in it-
+//   var items = document.querySelectorAll('div.item');
+//   var counter = 0;
+//   var amount = items.length;
+//   var current = items[0];
 
-// step 1- previous or next?
-// step 2 find the active item
-//step 3a if prev trigger an click on .item isActive previous sibling, prevSibling
-// step 3a if prev trigger a click on .item isActive previous sibling, prevSibling
-// 		3b if isActive is first in list, then do what? then find last .item
-// 	 	3c if next, trigger click on .item isActive nextSibling.
-//      3d if isActive is last in list then find first .item
+//   items.classList.add('isActive');
 
+//   function navigate(direction) {
+//     current.classList.remove('isActive');
+//     counter = counter + direction;
+//     if (direction === -1 && 
+//         counter < 0) { 
+//       counter = amount - 1; 
+//     }
+//     if (direction === 1 && 
+//         !items[counter]) { 
+//       counter = 0;
+//     }
+//     current = items[counter];
+//     current.classList.add('isActive');
+//   }
+//   next.addEventListener('click', function(ev) {
+//     navigate(1);
+//   });
+//   prev.addEventListener('click', function(ev) {
+//     navigate(-1);
+//   });
+//   navigate(0);
+// }) ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//___________________________________________
 });
 })(jQuery);
