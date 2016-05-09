add7fe9dab1f8f8ce6800c83d6668673bfa7ce3e
Amy Eckert <amy@amyeckertphoto.com>
2016-05-09 15:54:56 -0400

    turn off js for mobile screen

--- js/thumb-nav.js
+++ js/thumb-nav.js
@@ -1,8 +1,7 @@
 ;(function($) {
 
     $(document).ready(function() {
-
-        // init Masonry
+        // init Masory
         var $grid = $('.grid').masonry({
           itemSelector: '.grid-item',
           percentPosition: true,
@@ -13,189 +12,162 @@
           $grid.masonry();
         });
 
-        /*-------------------------------------------------------------------------------------------------
-                    previous or next button */
+        function thumbnailNav() {
+            /*-------------------------------------------------------------------------------------------------
+                        previous or next button */
 
-        $('.previous').on('click', function() {
-  
-            var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
-            var items = document.querySelectorAll('div.item');
+            $('.previous').on('click', function() {
+      
+                var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
+                var items = document.querySelectorAll('div.item');
 
-            //find the active item 
-            var activeImg = document.querySelector('div.isActive');
-            var lastImg = document.querySelector('div.thumbs-container').lastElementChild;  
-            var firstImg = document.querySelector('div.thumbs-container').firstElementChild;
-            var prevImg = document.querySelector('div.isActive').previousElementSibling;
+                //find the active item 
+                var activeImg = document.querySelector('div.isActive');
+                var lastImg = document.querySelector('div.thumbs-container').lastElementChild;  
+                var firstImg = document.querySelector('div.thumbs-container').firstElementChild;
+                var prevImg = document.querySelector('div.isActive').previousElementSibling;
 
-            $('.item.isActive').removeClass('isActive');
+                $('.item.isActive').removeClass('isActive');
 
-            //if activeImg is first image in list
-            if (activeImg == items[0]) {
+                //if activeImg is first image in list
+                if (activeImg == items[0]) {
 
-                //get the url to inject into .big-img-container
-                var chosen_thumb = $(lastImg).find('img').attr('src');
-                $(lastImg).addClass('isActive');
+                    //get the url to inject into .big-img-container
+                    var chosen_thumb = $(lastImg).find('img').attr('src');
+                    $(lastImg).addClass('isActive');
 
-                //make that img new big img.
-                $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                    //make that img new big img.
+                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
 
-            }
-            //else choose prev sibling
-            else {      
-                $(prevImg).addClass('isActive');
-                var chosen_thumb = $(prevImg).find('img').attr('src');
-                //make that img new big img.
-                $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
-            }
+                }
+                //else choose prev sibling
+                else {      
+                    $(prevImg).addClass('isActive');
+                    var chosen_thumb = $(prevImg).find('img').attr('src');
+                    //make that img new big img.
+                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                }
+                    
+                // console.log( mq );
+                });
+
+            $('.next').on('click', function() {
+
+                var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
+                var items = document.querySelectorAll('div.item');
                 
-            // console.log( mq );
+                // find the active item 
+                var activeImg = document.querySelector('div.isActive');
+                var lastImg = document.querySelector('div.thumbs-container').lastElementChild;  
+                var firstImg = document.querySelector('div.thumbs-container').firstElementChild;
+                var nextImg = document.querySelector('div.isActive').nextElementSibling;
+
+                $('.item.isActive').removeClass('isActive');
+
+                //if activeImg is first image in list
+                if(activeImg == items[0]) {
+
+                    //get the url to inject into .big-img-container
+                    var chosen_thumb = $(nextImg).find('img').attr('src');
+                    $(nextImg).addClass('isActive');
+
+                    //make that img new big img.
+                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                }
+
+                //if activeImg is last image in list
+                else if (activeImg === lastImg) {
+                    //get the url to inject into .big-img-container
+                    var chosen_thumb = $(firstImg).find('img').attr('src');
+                    $(firstImg).addClass('isActive');
+
+                    //make that img new big img.
+                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+
+
+                }
+                //else choose next sibling
+                else {      
+                    $(nextImg).addClass('isActive');
+                    var chosen_thumb = $(nextImg).find('img').attr('src');
+                    //make that img new big img.
+                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                }
+                console.log (nextImg)
             });
 
-        $('.next').on('click', function() {
+            /*-------------------------------------------------------------------------------------------------
+                        picks a thumbnail / hides thumbnail grid */
 
-            var imgCount = document.getElementById('thumbNav').getElementsByTagName('img').length;
-            var items = document.querySelectorAll('div.item');
-            
-            // find the active item 
-            var activeImg = document.querySelector('div.isActive');
-            var lastImg = document.querySelector('div.thumbs-container').lastElementChild;  
-            var firstImg = document.querySelector('div.thumbs-container').firstElementChild;
-            var nextImg = document.querySelector('div.isActive').nextElementSibling;
+            $('.item').on('click', function(e) {
+                e.preventDefault();
+                $('.item.isActive').removeClass('isActive');
+                $(this).addClass('isActive');
+                console.log(e);
 
-            $('.item.isActive').removeClass('isActive');
+                // Figure out which thumbnail is chosen
+                var chosen_thumb = $(this).find('img').attr('src');
+                var carousel = document.getElementById('carousel');
 
-            //if activeImg is first image in list
-            if(activeImg == items[0]) {
-
-                //get the url to inject into .big-img-container
-                var chosen_thumb = $(nextImg).find('img').attr('src');
-                $(nextImg).addClass('isActive');
-
-                //make that img new big img.
-                $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
-            }
-
-            //if activeImg is last image in list
-            else if (activeImg === lastImg) {
-                //get the url to inject into .big-img-container
-                var chosen_thumb = $(firstImg).find('img').attr('src');
-                $(firstImg).addClass('isActive');
-
-                //make that img new big img.
                 $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
 
+                // Change visibility of .thumbs to hidden and hide statement-body
+                var thumbs = document.getElementById('thumbNav');
+                var cNav = document.getElementById('cNav');
+                var imgInfo = document.getElementById('imgInfo');
+                var previous = document.getElementById('prevImg');
+                var next = document.getElementById('nextImg');
+                // var showHideStatement = document.querySelector('statement-body');
 
-            }
-            //else choose next sibling
-            else {      
-                $(nextImg).addClass('isActive');
-                var chosen_thumb = $(nextImg).find('img').attr('src');
-                //make that img new big img.
-                $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
-            }
-            console.log (nextImg)
-        });
+                carousel.style.display = 'block';
+                carousel.style.width = '75%';
+                 
+                bigImg.style.visibility = 'visible';
+                cNav.style.visibility = 'visible';
+                thumbs.style.display =  'none';
 
-        /*-------------------------------------------------------------------------------------------------
-                    picks a thumbnail / hides thumbnail grid */
+                imgInfo.style.visibility = 'hidden';
+                prevImg.style.visibility = 'visible';
+                nextImg.style.visibility = 'visible';
+                // showHideStatement.display = 'none';
 
-        $('.item').on('click', function(e) {
-            e.preventDefault();
-            $('.item.isActive').removeClass('isActive');
-            $(this).addClass('isActive');
-            console.log(e);
+                console.log(chosen_thumb, bigImg, imgInfo);
+            }); 
+            /*-------------------------------------------------------------------------------------------------
+                         hides big image/ shows thumbs grid */
+                            
+            $('.thumbs-only').on('click', function() {
 
-            // Figure out which thumbnail is chosen
-            var chosen_thumb = $(this).find('img').attr('src');
-            var carousel = document.getElementById('carousel');
+                var carousel = document.getElementById('carousel');
+                var showThumbs = document.getElementById('thumbNav');
+                var imgInfo = document.getElementById('imgInfo');
+                var previous = document.getElementById('prevImg');
+                var next = document.getElementById('nextImg');
+                var clNav = document.getElementById('cNav');
+                var grid = document.querySelector('grid');
+                // var showHideStatement = document.getElementById('statement-body');
+                
+                carousel.style.display = 'none';
+                showThumbs.style.display = 'block';
+                imgInfo.style.visibility = 'hidden';
+                cNav.style.visibility = 'hidden';
+                prevImg.style.visibility = 'hidden';
+                nextImg.style.visibility = 'hidden';
+                // showHideStatement.display = 'none';
 
-            $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+            });
+            /*-------------------------------------------------------------------------------------------------
+                  SHOW / HIDE STATEMENT click BUTTON  COMING SOON!! */
+        };
+        var width = screen.width;
+        var height = screen.height;
+        
+        // if ( width >= '450px' ) { 
 
-            //change visibility of .thumbs to hidden and hide statement-body
-            var thumbs = document.getElementById('thumbNav');
-            var cNav = document.getElementById('cNav');
-            var imgInfo = document.getElementById('imgInfo');
-            var previous = document.getElementById('prevImg');
-            var next = document.getElementById('nextImg');
-            // var showHideStatement = document.querySelector('statement-body');
+            // run thumbnailNav...
 
-            carousel.style.display = 'block';
-            carousel.style.width = '75%';
-             
-            bigImg.style.visibility = 'visible';
-            cNav.style.visibility = 'visible';
-            thumbs.style.display =  'none';
+        // } else ( do not );
 
-            imgInfo.style.visibility = 'hidden';
-            prevImg.style.visibility = 'visible';
-            nextImg.style.visibility = 'visible';
-            // showHideStatement.display = 'none';
-
-            // document.getElementById('showHide').addEventListener('click', function(e) {
-            // $('.statement-body').removeClass('showStatement');
-
-             // show image information 
-            // var imgInfo = document.querySelector('image-info');
-            // imgInfo.style.display = 'block';
-
-            console.log(chosen_thumb, bigImg, imgInfo);
-        }); 
-        /*-------------------------------------------------------------------------------------------------
-                     hides big image/ shows thumbs grid */
-                        
-        $('.thumbs-only').on('click', function() {
-
-            var carousel = document.getElementById('carousel');
-            var showThumbs = document.getElementById('thumbNav');
-            var imgInfo = document.getElementById('imgInfo');
-            var previous = document.getElementById('prevImg');
-            var next = document.getElementById('nextImg');
-            var clNav = document.getElementById('cNav');
-            var grid = document.querySelector('grid');
-            // var showHideStatement = document.getElementById('statement-body');
-            
-            carousel.style.display = 'none';
-            showThumbs.style.display = 'block';
-            imgInfo.style.visibility = 'hidden';
-            cNav.style.visibility = 'hidden';
-            prevImg.style.visibility = 'hidden';
-            nextImg.style.visibility = 'hidden';
-            // showHideStatement.display = 'none';
-
-        });
-        /*-------------------------------------------------------------------------------------------------
-                    SHOW / HIDE STATEMENT click BUTTON */
-
-      // document.getElementById('showHide').addEventListener('click', function(e) {
-      //   $('.statement-body.showStatement').removeClass('showStatement');
-      //   $('.statement-body').addClass('showStatement');
-
-        // var showHideStatement = document.getElementById('statement-body');
-          
-
-        // document.getElementsByTagName('button p').innerHTML = 'close';
-        // showHideStatement.style.display = 'block';
-        // showHideButton.innerHTML = 'close';
-
-        // console.log(showHideButton);
-        // });
-
-
-    //   $('.showHide').on('click', function(e) {
-    //      e.preventDefault();
-    //      $(this).addClass('showStatement');
-    //         // $('.item.isActive').removeClass('isActive');
-    //         // $(this).addClass('isActive');
-    //         console.log(e);
-
-    //     var showHideStatement = document.querySelector('statement-body');
-
-    //     showHideStatement.style.display = 'block';
-
-    //     console.log(e);
-
-
-    // });
 
 //________THE END______________________________________________________________________________________________________________
 });
