8ae1879651a30ac8dca4942758bb4355569569ab
Amy Eckert <amy@amyeckertphoto.com>
2017-04-03 15:58:11 -0400

    current image caption is updating correctly

--- js/main.js
+++ js/main.js
@@ -21,6 +21,9 @@
           $grid.masonry();
         }); 
 
+        //clear big image caption
+        $('.big-img-container > figcaption').remove();
+
         //  GLOBAL VARIABLES---------------------------//
         var width = screen.availWidth;
         var height = screen.availHeight;
@@ -34,49 +37,44 @@
         var cNav = document.getElementById('cNav');
         var grid = document.querySelector('.grid');
         var thumbs = document.getElementById('thumbGrid');
+        var newImageCaption = document.querySelector('.big-img-container');
 
-        // var addBackButton = function(e){
-        //     e.preventDefault();
-
-        //     var back = $('.project-name').previousSibling();
-        //     console.log(back);
-
-        // }();
 
         var imageInfo = function() {
+            //get contents of current image info <p>s
+            var chosenImageCaption = document.querySelector('.isActive > figcaption');
+            var chosenImageDetails = $('.isActive > figcaption').children(); //makes an array
+            var imageTitle = $(chosenImageDetails[0]).text();
+            var imageDate = $(chosenImageDetails[1]).text();
+            var imageDimensions = $(chosenImageDetails[2]).text();
+            var imageMedia = $(chosenImageDetails[3]).text();
+            var imageClient = $(chosenImageDetails[4]).text();
 
-            //get contents of image info <p>s
-            //add it to top of statement
+            $(newImageCaption).append('<figcaption class="image-info" id="imgInfo"><p class="title">' + imageTitle + '</p><p class="date">' + imageDate + '</p><p class="dimensions">' + imageDimensions + '</p><p class="media">' + imageMedia + '</p><p class="client">' + imageClient + '</p></figcaption>');   
+
         }
-
         //-------------- toggle statement text -----------------------------------//
         $('.statement').on('click', function(e) {
             e.preventDefault();
-            // statementBody.style.display = 'block';
             $(statementBody).toggle();
-            // console.log(statementBody);
         });
        
-        // for tablet and desktop ---------------------------------------------------//
+        // behaviour for tablet and desktop only ---------------------------------------------------//
         if (width >= 700 ) {
-
             $('.next').hide();
             $('.previous').hide();
             $('.thumbs-only').hide();
-
-
-            // $('.menu-side').prepend("<button class='btn-back'><a href='#'><img source='../images/arrow-left-grey.svg'</a></button>");
-                
-
-
-
+            
          /*--------------------------------
             when a thumbnail is clicked on-hides thumbnail grid 
-             -shows #carousel and carousel nav buttons-------------------------*/
+             -shows #carousel and carousel nav buttons, updates caption-------------------------*/
 
             $('.item').on('click', function(e) {
                 e.preventDefault();
-                
+                // empty big image caption info
+                $('.big-img-container > figcaption').remove();
+                // console.log(newImageCaption);
+
                 $('.thumbs-only').show();
                 $('.next').show();
                 $('.previous').show();
@@ -86,11 +84,14 @@
                 // console.log(e);
 
                 // Figure out which thumbnail is chosen
-                var chosen_thumb = $(this).find('img').attr('src');
+                var chosenThumb = $(this).find('img').attr('src');
                 var header = document.querySelector('header')
                 $('header').removeClass('nav-up').addClass('nav-down');
 
-                $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
+                
+                // change caption
+                imageInfo();
 
                 carousel.style.display = 'block';
                 carousel.style.order = 3;
@@ -115,6 +116,10 @@
                 previous or next button */
 
             $('.previous').on('click', function() {
+                //clear bigImage caption details
+                $('.big-img-container > figcaption').remove();
+                console.log(newImageCaption);
+
                 var imgCount = document.getElementById('thumbGrid').getElementsByTagName('img').length;
                 var items = document.querySelectorAll('figure.item');
 
@@ -130,22 +135,27 @@
                 if (activeImg == items[0]) {
 
                     //get the url to inject into .big-img-container
-                    var chosen_thumb = $(lastImg).find('img').attr('src');
+                    var chosenThumb = $(lastImg).find('img').attr('src');
                     $(lastImg).addClass('isActive');
 
                     //make that img new big img.
-                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                    $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
                 }
                 //else choose prev sibling
                 else {      
                     $(prevImg).addClass('isActive');
-                    var chosen_thumb = $(prevImg).find('img').attr('src');
+                    var chosenThumb = $(prevImg).find('img').attr('src');
                     //make that img new big img.
-                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
-                }                  
+                    $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
+                }
+
+                imageInfo();                  
             });
 
             $('.next').on('click', function() {
+                // remove the last figcaption 
+               $('.big-img-container > figcaption').remove();
+                console.log(newImageCaption);
 
                 var imgCount = document.getElementById('thumbGrid').getElementsByTagName('img').length;
                 var items = document.querySelectorAll('figure.item');
@@ -162,35 +172,38 @@
                 if(activeImg == items[0]) {
 
                     //get the url to inject into .big-img-container
-                    var chosen_thumb = $(nextImg).find('img').attr('src');
+                    var chosenThumb = $(nextImg).find('img').attr('src');
                     $(nextImg).addClass('isActive');
 
                     //make that img new big img.
-                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                    $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
                 }
 
                 //if activeImg is last image in list
                 else if (activeImg === lastImg) {
                     //get the url to inject into .big-img-container
-                    var chosen_thumb = $(firstImg).find('img').attr('src');
+                    var chosenThumb = $(firstImg).find('img').attr('src');
                     $(firstImg).addClass('isActive');
 
                     //make that img new big img.
-                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                    $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
                 }
                 //else choose next sibling
                 else {      
                     $(nextImg).addClass('isActive');
-                    var chosen_thumb = $(nextImg).find('img').attr('src');
+                    var chosenThumb = $(nextImg).find('img').attr('src');
                     //make that img new big img.
-                    $('.big-img-container').attr('style', 'background-image: url('+chosen_thumb+')');
+                    $('.big-img-container').attr('style', 'background-image: url('+chosenThumb+')');
                 }
-            });
-               
+                imageInfo();
+            });    
             /*-------------------------------------------------------------------------------------------------
                 hides big image/ shows thumbs grid */
                                 
             $('.thumbs-only').on('click', function() { 
+                // empty big image caption info
+                $('.big-img-container > figcaption').empty();
+
                 carousel.style.display = 'none';
                 showThumbs.style.display = 'block';
                 imgInfo.style.visibility = 'hidden';
